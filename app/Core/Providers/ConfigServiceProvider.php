c19a41663d7a81a4b3199e43be965152771adb7d
Darren Craig <darren@minus40.co>
2015-02-02 16:54:09 +0000

    Moving the Providers into 'Core'

--- app/Core/Providers/ConfigServiceProvider.php
+++ app/Core/Providers/ConfigServiceProvider.php
@@ -1,4 +1,4 @@
-<?php namespace App\Providers;
+<?php namespace App\Core\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
--- config/app.php
+++ config/app.php
@@ -139,11 +139,11 @@ return [
 		/*
 		 * Application Service Providers...
 		 */
-		'App\Providers\AppServiceProvider',
-		'App\Providers\BusServiceProvider',
-		'App\Providers\ConfigServiceProvider',
-		'App\Providers\EventServiceProvider',
-		'App\Providers\RouteServiceProvider',
+		'App\Core\Providers\AppServiceProvider',
+		'App\Core\Providers\BusServiceProvider',
+		'App\Core\Providers\ConfigServiceProvider',
+		'App\Core\Providers\EventServiceProvider',
+		'App\Core\Providers\RouteServiceProvider',
 
 	],
 
