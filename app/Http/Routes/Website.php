fdea7e3290e34afa9d6a3519afc0e9683e7f0140
Darren Craig <darren@minus40.co>
2015-02-03 10:07:57 +0000

    Work to gulp/js/css and boilerplate for basic pages

--- app/Http/Routes/Website.php
+++ app/Http/Routes/Website.php
@@ -5,3 +5,20 @@
  */
 
 $router->get('/', 'HomeController@index');
+
+
+
+
+
+
+
+/**
+ * Catch all routes for pages.
+ *
+ * Place all other routes above here.
+ */
+
+$router->get('{category}/{category}/{category}/{slug}', 'PagesController@show');
+$router->get('{category}/{category}/{slug}', 'PagesController@show');
+$router->get('{category}/{slug}', 'PagesController@show');
+$router->get('{slug}', 'PagesController@show');
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,43 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp'), sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minify = require('gulp-minify-css'), concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'), livereload = require('gulp-livereload'),
+    compass = require('gulp-compass'), path = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+var public_dir = "public_html/"
 
-elixir(function(mix) {
-    mix.less('app.less');
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('resources/assets/js/**/*.js', ['js']);
+    gulp.watch('resources/assets/scss/**/*.scss', ['css']);
+    gulp.watch('resources/views/**/*.blade.php').on('change', livereload.changed);
 });
+
+
+gulp.task('css', function () {
+    gulp.src([
+        'resources/assets/scss/app.scss'
+    ])
+    .pipe(compass({ css: public_dir + 'css', sass: 'resources/assets/scss' }))
+    .pipe(minify())
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(public_dir + 'css'))
+    .pipe(livereload());
+});
+
+gulp.task('js', function () {
+    gulp.src([
+        'resources/assets/js/vendor/jquery.js',  // always load jquery first
+        'resources/assets/js/helpers.js',
+        'resources/assets/js/vendor/**/*.js', // everything else
+        'resources/assets/js/app.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(public_dir + 'js'))
+    .pipe(livereload());
+});
+
+gulp.task('default', ['js', 'css', 'watch']);
